2bebfcb70c524dbc7bacf1ef257cead4c7bcaed5
Paul Edmunds <edmunds@gmail.com>
2018-01-10 22:06:35 +0000

    statecloud-sync.sh

--- statecloud-sync.sh
+++ statecloud-sync.sh
@@ -3,6 +3,6 @@ git add *.state*
 git add *.sav*
 git add .gitignore
 git commit -am "statecloud-sync.sh"
-git push origin master
-git pull origin master 
+git pull origin master
+git push origin master 
 
